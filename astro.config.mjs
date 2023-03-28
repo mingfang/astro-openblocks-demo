191f995aa4eddcac46ce9a192d442c130c4a18f7
Ming Fang <mingfang@mac.com>
2023-03-28 19:48:49 +0000

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  output: 'server',
+  adapter: node({
+    mode: "middleware",
+  }),
+  vite: {
+    ssr: {
+      noExternal: ["path-to-regexp"]
+    }
+  }
+});
